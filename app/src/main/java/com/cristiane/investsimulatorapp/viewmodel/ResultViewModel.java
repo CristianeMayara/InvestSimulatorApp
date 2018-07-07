62564c0c942dab40fa6742358a0ea04a58ca0650
Cristiane Mayara <cris.mayara@gmail.com>
2018-07-07 01:03:31 -0300

    Adding ViewModel for the Result screen

---
+++ app/src/main/java/com/cristiane/investsimulatorapp/viewmodel/ResultViewModel.java
@@ -0,0 +1,16 @@
+package com.cristiane.investsimulatorapp.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.cristiane.investsimulatorapp.model.Result;
+
+/**
+ * Created by cristiane on 07/07/2018.
+ */
+
+public class ResultViewModel extends ViewModel {
+    Result result;
+
+    private void loadResult() {
+    }
+}
