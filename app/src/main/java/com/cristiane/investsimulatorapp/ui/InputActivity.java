24ade3ada3211e6016cb5735fcc09b344748bfec
Cristiane Mayara <cris.mayara@gmail.com>
2018-07-11 23:41:09 -0300

    Including error feedback on Input Activity

--- app/src/main/java/com/cristiane/investsimulatorapp/ui/InputActivity.java
+++ app/src/main/java/com/cristiane/investsimulatorapp/ui/InputActivity.java
@@ -4,13 +4,16 @@ import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cristiane.investsimulatorapp.R;
 import com.cristiane.investsimulatorapp.model.Result;
@@ -23,7 +26,9 @@ import com.cristiane.investsimulatorapp.viewmodel.InputViewModel;
 @SuppressWarnings("deprecation")
 public class InputActivity extends AppCompatActivity implements LifecycleRegistryOwner {
 
-    public static final String TAG = "InputActivity";
+    public static final String TAG = InputActivity.class.getSimpleName();
+
+    private Context context;
     private EditText etValueInput;
     private EditText etDateInput;
     private EditText etCdiPercentageInput;
@@ -32,6 +37,7 @@ public class InputActivity extends AppCompatActivity implements LifecycleRegistr
     private InputViewModel model;
     private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
 
+    @NonNull
     @Override
     public LifecycleRegistry getLifecycle() {
         return this.lifecycleRegistry;
@@ -47,6 +53,7 @@ public class InputActivity extends AppCompatActivity implements LifecycleRegistr
     }
 
     private void initComponents() {
+        context = this;
         etValueInput = findViewById(R.id.et_value_input);
         etDateInput = findViewById(R.id.et_date_input);
         etCdiPercentageInput = findViewById(R.id.et_cdi_percentage_input);
@@ -64,10 +71,7 @@ public class InputActivity extends AppCompatActivity implements LifecycleRegistr
             @Override
             public void onChanged(@Nullable Result result) {
                 if (result != null) {
-                    //hideProgress();
                     openResultScreen(result);
-                } else {
-                    //showProgress();
                 }
             }
         });
@@ -78,11 +82,30 @@ public class InputActivity extends AppCompatActivity implements LifecycleRegistr
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.bt_simulate:
-                    model.loadResult(Double.parseDouble(etValueInput.getText().toString()), "CDI", Double.parseDouble(etCdiPercentageInput.getText().toString()), false, etDateInput.getText().toString());
+                    model.loadResult(context, etValueInput.getText().toString(), "CDI", etCdiPercentageInput.getText().toString(), false, etDateInput.getText().toString());
                     break;
             }
         }
     };
+
+    public void showValueInputError() {
+        etValueInput.setError(getString(R.string.error_invalid_value));
+        etValueInput.requestFocus();
+    }
+
+    public void showDateError() {
+        etDateInput.setError(getString(R.string.error_invalid_date));
+        etDateInput.requestFocus();
+    }
+
+    public void showCdiPercentageInputError() {
+        etCdiPercentageInput.setError(getString(R.string.error_invalid_rate));
+        etCdiPercentageInput.requestFocus();
+    }
+
+    public void showRegisterFailed() {
+        Toast.makeText(this, getString(R.string.error_simulation_failed), Toast.LENGTH_SHORT).show();
+    }
 
     private void openResultScreen(Result result) {
         Intent intent = new Intent(this, ResultActivity.class);
